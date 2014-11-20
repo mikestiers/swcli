340d85b7ace3339f4e1cefa15c7d48f9b1959950
Mike Stiers <mike.stiers@gmail.com>
2014-11-20 00:14:11 -0500

    a little more framework, day by day.  added todo list as well

--- swcli.py
+++ swcli.py
@@ -1,3 +1,13 @@
+# add format option to displayTicket() for single, multiple, etc
+# add curses support
+# figure out a good way to assign all items in a query to their column name in the database
+# check if command is numeric
+# add command line switches
+# is there a better way to construct command detection?  of course there is.
+# figure out a way to login via web interface.  it does not use a detectible auth according to curl
+# map out stored procedures for creating, closing, deleting, etc..
+# add option to open ticket in web interface
+
 import sqlite3
 import os
 import re
@@ -8,32 +18,33 @@ con = sqlite3.connect(dbname)
 cur = con.cursor()
 ticketNo = int
 
-#cur.execute("SELECT * FROM tickets WHERE id = '5'")
-#data = cur.fetchall()
-#print(data)
-
-# Command prompt
+# Main menu
 def getCommand():
     command = input("?>")
     if command == "q":
         quit()
     if command == "t":
         ticketMenu()
-    if command == "d":
-        showDue()
     else:
-        getCommand()
+        interHelp()
 
 # Ticket menu
 def ticketMenu():
-    ticketNo = input("ticket?>")
-    showTicket(ticketNo)
+    ticketCommand = input("ticket?>")
+    if ticketCommand == "d":
+        showDue()
+    if ticketCommand == "o":
+        showOpenTickets()
+    if ticketCommand == "q":
+        raise SystemExit
+    showTicket(ticketCommand)
 
 # Display interactive help menu
 def interHelp():
     print("Spiceworks Command Line Interface v", ver)
     print("(t)icket menu")
     print("(q)uit")
+    getCommand()
     return
 
 # Display command line help    
@@ -43,26 +54,40 @@ def cliHelp():
 
 # Display specific ticket information
 def showTicket(ticketNo):
-    # print(ticketNo)
-    # print(type(ticketNo))
     cur.execute("SELECT id, assigned_to, created_by, priority, closed_at, status, summary FROM tickets WHERE id =(?)", (ticketNo,))
     data = cur.fetchone()
     # print(data)
     if data == None:
         ticketMenu()
     else:
-        print("ID: ",data[0])
-        print("Assigned to: ",data[1])
-        print("Created by: ",data[2])
-        print("Priority: ",data[3])
-        print("Closed at: ",data[4])
-        print("Status: ",data[5])
-        print("Summary: ",data[6])
+        displayTicket(data)
         ticketMenu()
+    return
+
+# Display open tickets
+def showOpenTickets():
+    cur.execute("SELECT id, assigned_to, created_by, priority, closed_at, status, summary FROM tickets WHERE status ='open'")
+    data = cur.fetchall()
+    for x in data:
+        displayTicket(data[0])
+    return
+
+# Print ticket information
+def displayTicket(data):
+    print("--------------")
+    print("ID: ",data[0])
+    print("Assigned to: ",data[1])
+    print("Created by: ",data[2])
+    print("Priority: ",data[3])
+    print("Closed at: ",data[4])
+    print("Status: ",data[5])
+    print("Summary: ",data[6])
     return
 
 # Display tickets due
 def showDue():
+    cur.execute("SELECT id, assigned_to, created_by, priority, closed_at, status, summary FROM tickets WHERE status ='open'")
+    data = cur.fetchall()
     return
 
 # Quit
